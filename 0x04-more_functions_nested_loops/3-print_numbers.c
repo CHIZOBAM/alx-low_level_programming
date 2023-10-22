bd407e963ace72b1cd09f03de680a51ee76b5e55
CHIZOBAM <egwuenichizobam@gmail.com>
2023-10-21 22:53:18 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers ranging from 0 to 9.
+ */
+void print_numbers(void)
+{
+	char digit;
+
+	for (digit = 0; digit <= 9; digit++)
+	{
+		_putchar((digit % 10) + '0');
+	}
+
+	_putchar('\n');
+}
