470fca21f9931dc83ad5ae5216c86bf640eb2c6a
CHIZOBAM <egwuenichizobam@gmail.com>
2024-07-02 11:28:01 +0100

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ *	main - program that outputs using the puts function
+ *
+ *	return (0)
+ */
+
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle,");
+	return (0);	
+}
